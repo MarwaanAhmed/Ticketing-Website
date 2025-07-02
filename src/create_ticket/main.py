67676a4dabed783b3c7eb3eb88013b45c6ad45c7
marwan <ahmarwan20@gmail.com>
2025-07-02 22:24:33 +0100

    few touches to the main.py file. edited datetime import

--- src/create_ticket/main.py
+++ src/create_ticket/main.py
@@ -1,5 +1,9 @@
 import boto3
 import uuid
-import datatime
+import datetime
 import json
 import os
+
+dynamodb = boto3.resource('dynamodb')
+TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME')
+table = dynamodb.Table(TABLE_NAME)
