9b537855ad647b0a2362313b5dc054583c2cb2e2
marwan <ahmarwan20@gmail.com>
2025-07-03 10:27:41 +0100

    done part of the lambda function

--- src/create_ticket/main.py
+++ src/create_ticket/main.py
@@ -7,3 +7,17 @@ import os
 dynamodb = boto3.resource('dynamodb')
 TABLE_NAME = os.environ.get('DYNAMODB_TABLE_NAME')
 table = dynamodb.Table(TABLE_NAME)
+
+def lambda_handler(event, context): 
+
+    try:
+        body = json.loads(event['body'])
+
+        user_id = body['UserID']
+    except Exception as e :
+        print(f"Error: Could not parse UserID from request. Details: {e}")
+        
+        return{
+            'statusCode' : 400,
+            'body': json.dumps({'message': 'UserID is required and must be in the request body.'})
+        }
