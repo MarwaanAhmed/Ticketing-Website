86bd56cd76322cbdb5da91c3902b1cb47dd16349
marwan <ahmarwan20@gmail.com>
2025-07-12 20:21:13 +0100

    Added the IAM role

---
+++ modules/lambda_functions/main.tf
@@ -0,0 +1,22 @@
+# IAM Role
+
+resource "aws_iam_role" "lambda_function" {
+  name = "TicketCreatorRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "This function allows Lambda to "
+  }
+}
