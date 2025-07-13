7cafc3e6d2f9051a45aa9d73a30612955484c009
marwan <ahmarwan20@gmail.com>
2025-07-13 10:19:53 +0100

    updated the outputs.tf to output the ARN

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -2,3 +2,8 @@ output "table_name" {
   description = "The name of the DynamoDB table."
   value       = aws_dynamodb_table.ticket_table.name
 }
+
+output "table_arn" {
+  description = "this will output the ARN of the dynamodb table"
+  value       = aws_dynamodb_table.ticket_table.arn
+}
