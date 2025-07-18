c78f0b07d5ab75db369d547eb5ff5f98f9e9cbad
marwan <ahmarwan20@gmail.com>
2025-07-18 15:40:29 +0100

    added the route resource block for my api

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,10 +1,17 @@
-resource "aws_apigatewayv2_api" "ticket_http_api" {
+resource "aws_apigatewayv2_api" "create_ticket_api" {
   name          = var.api_name
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_integration" "create_ticket_integration" {
-  api_id           = aws_apigatewayv2_api.ticket_http_api.id
+  api_id           = aws_apigatewayv2_api.create_ticket_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = var.lambda_arn
+}
+
+resource "aws_apigatewayv2_route" "create_ticket_route" {
+  api_id    = aws_apigatewayv2_api.create_ticket_api.id
+  route_key = "POST /tickets"
+
+  target = "integrations/${aws_apigatewayv2_integration.create_ticket_integration.id}"
 }
