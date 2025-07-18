a33a0de5245944be70515d5916cbe4e134bb6522
marwan <ahmarwan20@gmail.com>
2025-07-18 12:05:45 +0100

    Added the integration resource block

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -2,3 +2,9 @@ resource "aws_apigatewayv2_api" "ticket_http_api" {
   name          = var.api_name
   protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_integration" "create_ticket_integration" {
+  api_id           = aws_apigatewayv2_api.ticket_http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.lambda_arn
+}
