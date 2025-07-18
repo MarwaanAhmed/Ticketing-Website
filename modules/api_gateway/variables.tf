ff2a23694d310ca010bdd10df7304d7bf7240c0a
marwan <ahmarwan20@gmail.com>
2025-07-18 12:06:16 +0100

    Added the lambda ARN varible block

--- modules/api_gateway/variables.tf
+++ modules/api_gateway/variables.tf
@@ -1,4 +1,9 @@
 variable "api_name" {
   description = "the name for the http api"
-  type = "string"
+  type = string
+}
+
+variable "lambda_arn" {
+  description = "The ARN of the Lambda function to be triggered."
+  type = string
 }
